c32b201c5d380bf3c8d1598694765c0bf980c980
MarianK7 <marian.keszi@gmail.com>
2024-09-29 10:17:41 +0200

    Class inheritance

---
+++ src/java_basics/Bird.java
@@ -0,0 +1,12 @@
+package java_basics;
+
+public class Bird extends Animal{
+
+    public Bird(String type, String name, int age, int numberOfLegs, boolean winged, java_basics.eatingHabits eatingHabits) {
+        super(type, name, age, numberOfLegs, winged, eatingHabits);
+    }
+
+    public void fly(){
+        System.out.println("Brd is flying...");
+    }
+}
--- src/java_basics/Zoo.java
+++ src/java_basics/Zoo.java
@@ -7,6 +7,7 @@ public class Zoo {
         Animal Dog = new Animal("Dog", "Blue", 4, 4, false, eatingHabits.Carnivore);
         Animal Rhino = new Animal("Rhino", "Jeremy", 15, 4, false, eatingHabits.Herbivore);
         Animal Hooter =  new Animal("Hooter", "Hunter", 10, 2, true, eatingHabits.Carnivore);
+        Bird Bird = new Bird("Bird", "Papagaj", 3, 2, true, eatingHabits.Carnivore);
 
         Dog.describe();
         Rhino.describe();
@@ -14,5 +15,6 @@ public class Zoo {
         Rhino.isWalking();
         Dog.age = 10;
         Dog.describe();
+        Bird.fly();
     }
 }
